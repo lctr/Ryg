98dd747023256eea4aacff3bdddd16ce267bbd2b
Lictor Guzman <lctrgzmn@gmail.com>
2021-08-29 04:11:23 -0500

    inspecting tokens

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,29 @@ use lexer::Lexer;
 use token::Token;
 
 fn main() {
-  let src = String::from("hello, world!");
-
+  let src = String::from("print \"world\"");
+  // let src = String::from("do");
   inspect_tokens(&src);
 }
 
 fn inspect_tokens(src: &str) {
   let mut lexer = Lexer::new(src);
-  let mut token;
+  let mut token = lexer.peek();
   // let mut done = false;
   println!("{:?}", src);
   while !lexer.eof() {
-    token = lexer.next();
-    println!("{:?}", token);
-
+    println!("{:?}", lexer.next());
     // if this prints, then lexer.eof fails somehow
     if token.eq(&Token::Empty()) {
       println!("EOF FAIL");
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_works() {
+    assert_eq!(2 + 2, 4);
   }
 }
